c619e2ed99d8aea37faacdb772df4a48eefa1e5d
iabthao <60857747+iabthao@users.noreply.github.com>
2020-08-07 15:36:38 -0500

    Heart Rate calculator assignment

---
+++ HeartTargetRateCalculator/HeartTargetRateCalculator/HeartRates.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeartTargetRateCalculator
+{
+    class HeartRates
+    {
+        private string firstName;
+        private string lastName;
+        private int yearOfBirth;
+        private int currentYear;
+        
+
+        //set a constructor
+
+        public HeartRates(string fName, string lName, int yOfbirth, int cYear)
+        {
+            FirstName = fName;
+            LastName = lName;
+            YearOfBirth = yOfbirth;
+            currentYear = cYear;
+
+        }
+        //get and set lines for variables
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public int YearOfBirth
+        {
+            get
+            {
+                return yearOfBirth;
+            }
+            set
+            {
+                yearOfBirth = value;
+            }
+
+        }
+        // Calculate and return the person's age (in years)
+
+        public int PersonAge
+        {
+            get
+            {
+                return (currentYear - yearOfBirth);
+            }
+        }
+
+        // Property that calculates the maximum heart rate
+        public double MaxRates
+
+        {
+            get { return (220 - PersonAge); }
+        }
+
+        // property that calculates the minimum and maximun target heart rates
+
+        public double MinHeartRates
+        {
+            get { return MaxRates * 0.50; }
+        }
+        public double MaxHeartRates
+        {
+            get { return MaxRates * 0.85; }
+
+        }
+    }
+}
---
+++ HeartTargetRateCalculator/HeartTargetRateCalculator/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace HeartTargetRateCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //set a title            
+            Console.Title = ("Heart Rate Calculator");
+            
+            //ask for firstName
+            Console.WriteLine("Please enter your first name:");
+            string firstName = Console.ReadLine();
+
+            //last name
+            Console.WriteLine("Please enter your last name:");
+            string lastName = Console.ReadLine();
+
+            //year of birth
+            Console.WriteLine("Please enter your year of birth:");
+            int yearbirth = int.Parse(Console.ReadLine());
+
+            //Check value, can't be negative or lesser than 0, prompt reenter
+
+            while (yearbirth <= 0)
+            {
+
+                Console.WriteLine("Please enter a valid year of birth");
+                Console.WriteLine("Enter your year of birth: ");
+                yearbirth = int.Parse(Console.ReadLine());
+            }
+
+            //get year
+
+            int currentYear = DateTime.Now.Year;
+
+            //instantiate
+
+            HeartRates heartrates = new HeartRates(firstName, lastName, yearbirth, currentYear);
+
+            //display content
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Hi {firstName} {lastName},");
+
+            Console.WriteLine($"You are {heartrates.PersonAge} years old.");
+
+            //display max rates
+
+            Console.WriteLine($"Your maximum heart rate is {heartrates.MaxRates} beats per minute.");
+
+            //displays min & maximum heart rate for target-rate range
+
+            Console.WriteLine($"Your recommended target-heart-range is {Convert.ToInt32(heartrates.MinHeartRates)} to {Convert.ToInt32(heartrates.MaxHeartRates)} beats per minute.");
+            
+            
+        }
+    }
+}
